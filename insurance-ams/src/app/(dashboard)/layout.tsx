ad7ef6c08d806740fa896f4b72efa9a90c7b0bdb
StringKs <kent.phipps@ufl.edu>
2025-02-21 22:59:55 -0500

    started till nav bar

---
+++ insurance-ams/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from '@/Component/Menu';
+
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div className="h-screen flex">
+            {/* Left Side */}
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] cl:w-[14%] bg-red-200 p-4">
+                <Link 
+                    href="/"
+                    className="flex items-center justify-center lg:justify-start gap-2"
+                >
+                    <Image src="/logo.png" alt="logo" width={30} height={30}/>
+                    <span className="hidden lg:block">Insurance AMS</span>
+                </Link>
+                <Menu/>
+            </div>
+            {/* Right Side */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] cl:w-[86%] bg-blue-300">Right</div>
+        </div>
+    );
+  }
+  
--- insurance-ams/tailwind.config.ts
+++ insurance-ams/tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/\\(dashboard\\)/**/*.{js,ts,jsx,tsx,mdx}",  // added-couldnt find dashboard
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
